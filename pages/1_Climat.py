39edda761809a360c2dbaa60e583df8aa9322574
jvu <julienvu@interoc.fr>
2026-05-11 17:08:02 +0200

    modif taille fenêtre graphique

--- pages/1_Climat.py
+++ pages/1_Climat.py
@@ -889,7 +889,7 @@ def build_scenario_table(df_proj_period: pd.DataFrame, selected_zones: list[int]
 
 
 def plot_scenario_comparison(df_table, indicator, period, scenario):
-    fig, ax = plt.subplots(figsize=(11, 5))
+    fig, ax = plt.subplots(figsize=(15, 5))
     df = df_table[df_table["scenario"] == scenario].copy()
     sorted_zones = sorted(df["zone"].unique())
     x = np.arange(len(sorted_zones))
